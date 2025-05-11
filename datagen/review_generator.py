3a26942bcaa8a57d04f3b8121f11e0b2bee6f357
Panos Kaf <panoskatsalifis@gmail.com>
2025-05-11 22:13:06 +0300

    reviews

--- datagen/review_generator.py
+++ datagen/review_generator.py
@@ -21,9 +21,9 @@ for performance_id in range(1, total_performances + 1):
 
     # Randomly have some of those visitors review the performance
     for visitor_id in range(start_visitor_id, end_visitor_id):
-        if random.random() < 0.2:  # 20% chance to leave reviews
+        if random.random() < 0.1:  # 20% chance to leave reviews
             for criteria_id in range(1, 6):  # 5 criteria
-                if random.random() < 0.5:  # 50% chance per criteria
+                if random.random() < 0.2:  # 50% chance per criteria
                     score = random.randint(1, 5)
                     values.append(f"({score}, {criteria_id}, {visitor_id}, {performance_id})")
 
